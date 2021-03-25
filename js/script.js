9fa1b935b342cbe0ab102d99f8355914839a9bd1
Rajan Tiwari <rajan@leadschool.in>
2021-03-25 20:01:49 +0530

    info

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// sign in //
-var modal = document.getElementById('id01');
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// sign up//
-var modal = document.getElementById('id02');
-      
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 // slideshow //
 
