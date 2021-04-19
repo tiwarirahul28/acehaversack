161775ee985271b00a2bafd40f97d61b464836b3
Rajan Tiwari <rajan@leadschool.in>
2021-04-19 14:56:21 +0530

    updated navbar & added bottom to top btn

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
-
-// slideshow //
-
-var slideIndex = 1;
-    showSlides(slideIndex);
-    
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-    
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("demo");
-      var captionText = document.getElementById("caption");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
+  //javascript for navigation bar effects on scroll
+  window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+  });
+  
+  //javascript for responsive navigation sidebar menu
+  const menuBtn = document.querySelector(".menu-btn");
+  const navigation = document.querySelector(".navigation");
+  const navigationItems = document.querySelectorAll(".navigation a")
+  
+  menuBtn.addEventListener("click",  () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+  });
+  
+  navigationItems.forEach((navigationItem) => {
+    navigationItem.addEventListener("click", () => {
+      menuBtn.classList.remove("active");
+      navigation.classList.remove("active");
+    });
+  });
+  
+  //javascript for scroll to top button
+  const scrollBtn = document.querySelector(".scrollToTop-btn");
+  
+  window.addEventListener("scroll", function(){
+    scrollBtn.classList.toggle("active", window.scrollY > 500);
+  });
+  
+  //javascript for scroll back to top on click scroll-to-top button
+  scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+  
+  //javascript for reveal website elements on scroll
+  window.addEventListener("scroll", reveal);
+  
+  function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for(var i = 0; i < reveals.length; i++){
+      var windowHeight = window.innerHeight;
+      var revealTop = reveals[i].getBoundingClientRect().top;
+      var revealPoint = 50;
+  
+      if(revealTop < windowHeight - revealPoint){
+        reveals[i].classList.add("active");
       }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-      captionText.innerHTML = dots[slideIndex-1].alt;
     }
+  }
+  
